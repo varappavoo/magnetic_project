d40d69c7102f02aaaa93a767a7cdfe9b0fd3dffd
vara <p.appavoo@gmail.com>
2018-10-08 01:39:18 +0800

    plot delimited area and location of unknown sensor

--- magneto/plot_sys_of_eqn.py
+++ magneto/plot_sys_of_eqn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import numpy as np
+import matplotlib.patches as patches
 # import matplotlib.pyplot as plt
 from pylab import *
 # from math import *
@@ -44,19 +45,43 @@ def equation_of_line_given_beacon_degree(beacon, theta):
 	else:
 		m=0
 		c=0
-	print(m,c)
+	# print(m,c)
 	return m,c
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-x = linspace(-250, 250, 500)
-ax.set_ylim(-250,250)
+x = linspace(-300, 300, 600)
+limit=300
+ax.set_xlim(-limit,limit)
+ax.set_ylim(-limit,limit)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
+
+rect = patches.Rectangle((-250,-250),500,500,linewidth=1,edgecolor='black',facecolor='none',linestyle=":")
+ax.add_patch(rect)
+ax.set_aspect('equal')
+# ax.axis('equal')
+# su=[]
+
 sx=[0,-50, 100, 0,   150, 100,  50, 200, -200,-200]
 sy=[0, 50, 50,  150, 150,-100,-150, -200,-100, 250]
+colors={}
+colors['b1']='tomato'
+colors['b2']='blue'
+colors['b3']='olive'
+colors['b4']='purple'
 
+bx=[-250	,0		,250	,0]
+by=[0		,-250	,0		,250]
 
+A=[]
+B=[]
+
+count_beacons={}
+
+for i in range(4):
+	ax.text(bx[i], by[i], "B"+str(i+1))
+	count_beacons['b'+str(i+1)]=0
 
 mydata = np.genfromtxt('experiment_201810_0405/RESULTS/mydata.csv', delimiter=',', dtype=None, names=('beacon', 'u_sensor', 'b_sensor', 'tstart', 'tend', 'degree'))
 for i in range(0,len(mydata)):
@@ -64,25 +89,45 @@ for i in range(0,len(mydata)):
 	sensor = str(mydata[i]['u_sensor'])
 	beacon = beacon[2:4]
 	sensor = sensor[2:sensor.rfind("'")]
-	if(beacon==b and sensor==s):
-		print(mydata[i])
-		m,c = equation_of_line_given_beacon_degree(beacon, mydata[i]['degree'])
-		ax.plot(x, (m*x)+c, alpha=0.4)#, label="y = x**2")
-		# print(str(mydata[i]['beacon'])[2:4])
-		# print(str(mydata[i]['beacon'])[2:4]=='b1')
-	elif(b=="all" and sensor==s):
-		print(mydata[i])
-		m,c = equation_of_line_given_beacon_degree(beacon, mydata[i]['degree'])
-		ax.plot(x, (m*x)+c, alpha=0.4)#, label="y = x**2")
 
-ax.scatter(sx,sy)
+	# print(beacon,sensor, mydata[i])
+	# if(beacon!=''):
+		
+
+	if(beacon==b and sensor==s):
+		m,c = equation_of_line_given_beacon_degree(beacon, mydata[i]['degree'])
+		ax.plot(x, (m*x)+c, alpha=0.4, color=colors[beacon])#, label="y = x**2")
+		count_beacons[beacon] = count_beacons[beacon] + 1
+
+	elif(b=="all" and sensor==s):
+		if(beacon in ['b1','b4','b4','b4']):
+			m,c = equation_of_line_given_beacon_degree(beacon, mydata[i]['degree'])
+			ax.plot(x, (m*x)+c, alpha=0.4, color=colors[beacon])#, label="y = x**2")
+
+			A.append([1, -m])
+			B.append(c)
+			count_beacons[beacon] = count_beacons[beacon] + 1
+
+# print(A,"\n",B)
+# if(b=="all" and sensor==s):
+x = np.linalg.lstsq(A, B, rcond=-1)
+u_y = x[0][0]
+u_x = x[0][1]
+# print(x)
+print(x,x[0])
+
+ax.scatter(sx,sy,marker='x', color='blue')
+ax.scatter(bx,by,marker='o', color='green')
+
+for i in range(10):
+	ax.text(sx[i]+10, sy[i]-10, "s"+str(i+1))
+
+
+
+
+ax.scatter(np.array([u_x]),np.array([u_y]),marker='*', color='red')#,c=[100])
+ax.text(np.array([u_x])+10,np.array([u_y])+10, s, color='red')#,c=[100])
+
+print(count_beacons)
 plt.show()
 
-
-
-# ax.plot(x, x**2, label="y = x**2")
-# ax.plot(x, x**3, label="y = x**3")
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_title('title')
-# ax.legend(loc=2); # upper left corner
